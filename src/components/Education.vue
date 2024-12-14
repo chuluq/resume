9c0469ad32ae91770841a00036decca7c2bd1e39
chuluq <chuluq@arvis.id>
2024-12-14 17:20:52 +0700

    chore: add favicon and adjust copywriting

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -10,7 +10,7 @@ import { educations } from "@/config/education";
 </script>
 <template>
   <section class="space-y-6">
-    <h1 class="font-semibold text-lg border-b-2 text-main border-main w-fit">Experience</h1>
+    <h1 class="font-semibold text-lg border-b-2 text-main border-main w-fit">Education</h1>
     <div class="flex flex-col gap-y-4 min-w-fit">
       <div v-for="education in educations" :key="education.id" class="flex gap-x-4 items-center">
         <p class="font-medium flex-1 text-right text-[8px] text-[#292929]">{{ education.year }}</p>

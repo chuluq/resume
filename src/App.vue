7d1e024f302a63bccf4e014bc2fa2671eeb4121e
chuluq <chuluq@arvis.id>
2024-12-14 07:50:47 +0700

    feat: add sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Sidebar from "./components/Sidebar.vue";
+</script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <main class="container mx-auto max-w-screen-sm my-4 flex gap-x-2">
+    <Sidebar />
+    <section class="py-10 px-6">hello</section>
+  </main>
 </template>
---
+++ src/config/personal-info.ts
@@ -0,0 +1,78 @@
+export const personalData: {
+  id: number;
+  label: string;
+  data: string;
+}[] = [
+  {
+    id: 1,
+    label: "Location",
+    data: "Pekalongan, Indonesia",
+  },
+  {
+    id: 2,
+    label: "Phone",
+    data: "(+62) 878 9471 0683",
+  },
+  {
+    id: 3,
+    label: "Email",
+    data: "choirulchuluq10@gmail.com",
+  },
+  {
+    id: 4,
+    label: "Github ID",
+    data: "@chuluq",
+  },
+  {
+    id: 5,
+    label: "Website",
+    data: "glowing-glasses.vercel.app",
+  },
+];
+
+export const skills = [
+  {
+    id: 1,
+    data: "HTML",
+  },
+  {
+    id: 2,
+    data: "CSS",
+  },
+  {
+    id: 3,
+    data: "TailwindCSS",
+  },
+  {
+    id: 4,
+    data: "JavaScript",
+  },
+  {
+    id: 5,
+    data: "TypeScript",
+  },
+  {
+    id: 6,
+    data: "React.js",
+  },
+  {
+    id: 7,
+    data: "Next.js",
+  },
+  {
+    id: 8,
+    data: "React Native",
+  },
+  {
+    id: 9,
+    data: "Redux",
+  },
+  {
+    id: 10,
+    data: "Vue.js",
+  },
+  {
+    id: 11,
+    data: "Node.js",
+  },
+];

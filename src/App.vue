7b5b4a1abac62d063160cd1bebadadea82af71d0
chuluq <chuluq@arvis.id>
2024-12-14 07:54:10 +0700

    chore: adjust tag

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@ import Sidebar from "./components/Sidebar.vue";
 </script>
 
 <template>
-  <main class="container mx-auto max-w-screen-sm my-4 flex gap-x-2">
+  <div class="container mx-auto max-w-screen-sm my-4 flex gap-x-2">
     <Sidebar />
-    <section class="py-10 px-6">hello</section>
-  </main>
+    <main class="py-10 px-6">hello</main>
+  </div>
 </template>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -13,20 +13,20 @@ import { personalData, skills } from "@/config/personal-info";
       <img src="../assets/img/profile.png" alt="profile" />
     </div>
     <div class="px-[27.5px] flex flex-col gap-y-8">
-      <div class="text-center space-y-2">
+      <section class="text-center space-y-2">
         <div v-for="item in personalData" :key="item.id">
           <h3 class="font-bold text-[10px] leading-[15px] text-[#292929]">{{ item.label }}</h3>
           <h3 class="font-medium text-[10px] leading-[15px] text-[#292929]">{{ item.data }}</h3>
         </div>
-      </div>
-      <div class="gap-y-4 flex flex-col items-center justify-center">
+      </section>
+      <section class="gap-y-4 flex flex-col items-center justify-center">
         <h1 class="font-semibold text-lg border-b-2 text-[#292929] border-[#292929]">Languages</h1>
         <div class="space-y-1 text-center">
           <h3 class="font-medium text-[10px] leading-[15px] text-[#292929]">Indonesia</h3>
           <h3 class="font-medium text-[10px] leading-[15px] text-[#292929]">English</h3>
         </div>
-      </div>
-      <div class="gap-y-4 flex flex-col items-center justify-center">
+      </section>
+      <section class="gap-y-4 flex flex-col items-center justify-center">
         <h1 class="font-semibold text-lg border-b-2 text-[#292929] border-[#292929]">Skills</h1>
         <div class="space-y-1 text-center">
           <h3
@@ -37,7 +37,7 @@ import { personalData, skills } from "@/config/personal-info";
             {{ skill.data }}
           </h3>
         </div>
-      </div>
+      </section>
     </div>
   </aside>
 </template>
